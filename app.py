2bbb6bcc8abe003dbcb76191315616da6cbf90bc
Cha0sai <ofgnib@gmail.com>
2025-04-12 23:40:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 import os
+import json
 
 app = Flask(__name__)
 
@@ -9,7 +10,15 @@ def home():
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    return 'OK'
+    # 確認是來自 LINE
+    signature = request.headers.get('X-Line-Signature')
+    body = request.get_data(as_text=True)
+    
+    print(f"Header: {signature}")
+    print(f"Body: {body}")
+    
+    # 簡易回應
+    return 'OK', 200
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
